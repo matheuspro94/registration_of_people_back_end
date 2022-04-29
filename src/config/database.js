d25dbd188de3777677532c1307d40bb7d9b12d0f
mroenca40 <mproenca.marketing@gmail.com>
2022-04-28 23:29:32 -0300

    add start

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,10 @@ require('dotenv').config()
 
 module.exports = {
   dialect: 'mysql',
-  host: process.env.MYSQL_HOST ||'localhost',
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD ||'password',
-  database: process.env.MYSQL_DB_NAME ||'registration_of_people',
+  host: process.env.MYSQL_HOST ||'us-cdbr-east-05.cleardb.net',
+  username: process.env.MYSQL_USER || 'bdb85667aefc17',
+  password: process.env.MYSQL_PASSWORD ||'da978359',
+  database: process.env.MYSQL_DB_NAME ||'heroku_8aa08857f020dd7',
   define: {
     timestamps: true,
     underscored: true,
